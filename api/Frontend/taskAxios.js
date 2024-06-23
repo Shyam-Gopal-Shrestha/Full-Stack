a52086c6d4f8fd7c2d1998161a022bca802b0559
Shyam-Gopal-Shrestha <shyamshrestha1003@gmail.com>
2024-06-23 14:55:03 +1000

    re build

--- api/Frontend/taskAxios.js
+++ api/Frontend/taskAxios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_URL = import.meta.env.VITE_API_URL
+const API_URL = import.meta.env.PROD ? 'https://full-stack-i4cb.onrender.com/api/tasks' : "http://localhost:8000/api/tasks"
 
 // GET a task |  GET request | read
 export const getTasks = () => {
